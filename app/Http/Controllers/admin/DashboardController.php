bf25d6f7ceb1f4f365a3ae61989a1d7ca8e072b5
moan <moanfs1@gmail.com>
2023-11-19 09:08:04 +0700

    update

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -8,19 +8,27 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $jumlahuser =  User::all()->count();
+        $users = User::select(DB::raw("COUNT(*) as count"), DB::raw("MONTHNAME(created_at) as month_name"))
+            ->whereYear('created_at', date('Y'))
+            ->groupBy(DB::raw("Month(created_at)"))
+            ->pluck('count', 'month_name');
+
+        $labels = $users->keys();
+        $data = $users->values();
         // dd($jumlahuser);
         return view('admin.dashboard', [
             'userlogin' => User::where('id', auth()->id())->first(),
             'jumlahusers'    => $jumlahuser,
             'jumlahproduk'  => Product::all()->count(),
             'jumlahkategori' => Category::all()->count(),
-            'jumlahbrand'   => Brand::all()->count()
+            'jumlahbrand'   => Brand::all()->count(),
         ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -43,7 +43,7 @@
                         </div>
                     </div>
 
-                    <div id="column-chart"></div>
+                    <div id="myChart"></div>
                 </div>
 
 
@@ -84,144 +84,37 @@
     </div>
     <script type="text/javascript">
         // ApexCharts options and config
-        window.addEventListener("load", function() {
-            const options = {
-                colors: ["#1A56DB", "#FDBA8C"],
-                series: [{
-                        name: "Organic",
-                        color: "#1A56DB",
-                        data: [{
-                                x: "Mon",
-                                y: 231
-                            },
-                            {
-                                x: "Tue",
-                                y: 122
-                            },
-                            {
-                                x: "Wed",
-                                y: 63
-                            },
-
-                        ],
-                    },
-                    {
-                        name: "Social media",
-                        color: "#FDBA8C",
-                        data: [{
-                                x: "Mon",
-                                y: 232
-                            },
-                            {
-                                x: "Tue",
-                                y: 113
-                            },
-                            {
-                                x: "Wed",
-                                y: 341
-                            },
-
-                        ],
-                    },
-                    {
-                        name: "Social media",
-                        color: "#98E695",
-                        data: [{
-                                x: "Mon",
-                                y: 232
-                            },
-                            {
-                                x: "Tue",
-                                y: 113
-                            },
-                            {
-                                x: "Wed",
-                                y: 341
-                            },
-
-                        ],
-                    },
-                ],
-                chart: {
-                    type: "bar",
-                    height: "320px",
-                    fontFamily: "Inter, sans-serif",
-                    toolbar: {
-                        show: false,
-                    },
-                },
-                plotOptions: {
-                    bar: {
-                        horizontal: false,
-                        columnWidth: "70%",
-                        borderRadiusApplication: "end",
-                        borderRadius: 8,
-                    },
-                },
-                tooltip: {
-                    shared: true,
-                    intersect: false,
-                    style: {
-                        fontFamily: "Inter, sans-serif",
-                    },
-                },
-                states: {
-                    hover: {
-                        filter: {
-                            type: "darken",
-                            value: 1,
-                        },
-                    },
-                },
-                stroke: {
-                    show: true,
-                    width: 0,
-                    colors: ["transparent"],
-                },
-                grid: {
-                    show: false,
-                    strokeDashArray: 4,
-                    padding: {
-                        left: 2,
-                        right: 2,
-                        top: -14
-                    },
-                },
-                dataLabels: {
-                    enabled: false,
-                },
-                legend: {
-                    show: false,
-                },
-                xaxis: {
-                    floating: false,
-                    labels: {
-                        show: true,
-                        style: {
-                            fontFamily: "Inter, sans-serif",
-                            cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
-                        }
-                    },
-                    axisBorder: {
-                        show: false,
-                    },
-                    axisTicks: {
-                        show: false,
-                    },
-                },
-                yaxis: {
-                    show: false,
-                },
-                fill: {
-                    opacity: 1,
-                },
+        var labels = {
+            {
+                Js::from($labels)
             }
-
-            if (document.getElementById("column-chart") && typeof ApexCharts !== 'undefined') {
-                const chart = new ApexCharts(document.getElementById("column-chart"), options);
-                chart.render();
+        };
+        var users = {
+            {
+                Js::from($data)
             }
-        });
+        };
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: users,
+            }]
+        };
+
+        const config = {
+            type: 'line',
+            data: data,
+            options: {}
+        };
+
+        const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
     </script>
 
 </x-admin-layout>
